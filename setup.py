f77ddb46465fb9f6fc88b4971fb7fc58591888a8
David McClure <davidwilliammcclure@gmail.com>
2015-09-11 14:51:51 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='d2g-corrections',
+    version='0.0.0',
+    description='Correcting names in d3g.',
+    license='Apache-2.0',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    scripts=['bin/d2gc'],
+    packages=find_packages(),
+
+    install_requires=[],
+
+)
